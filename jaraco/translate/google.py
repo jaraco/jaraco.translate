62dcfb67a69673cc99b299ba876858057d0de3c5
Jason R. Coombs <jaraco@jaraco.com>
2014-07-16 13:08:05 -0400

    Raise error results

--- jaraco/translate/google.py
+++ jaraco/translate/google.py
@@ -7,11 +7,12 @@ def translate(text, target_lang='en', source_lang=None):
 	"""
 	url_base = 'https://www.googleapis.com/language/translate/v2'
 	params = dict(
-		key = translate.API_key,
-		q = text,
-		target = target_lang,
+		key=translate.API_key,
+		q=text,
+		target=target_lang,
 	)
 	if source_lang:
 		params['source'] = source_lang
 	resp = requests.get(url_base, params=params)
+	resp.raise_for_status()
 	return resp.json()['data']['translations'][0]['translatedText']
