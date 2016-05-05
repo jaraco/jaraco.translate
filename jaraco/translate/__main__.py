d132171488e490797f4ac8b83ce279983c41833d
Jason R. Coombs <jaraco@jaraco.com>
2016-05-05 12:24:25 -0400

    Fix test failure in collection

--- jaraco/translate/__main__.py
+++ jaraco/translate/__main__.py
@@ -3,8 +3,13 @@ import os
 
 from . import google
 
-lang = sys.argv[1]
-text = ' '.join(sys.argv[2:])
 
-google.translate.API_key = os.environ['GOOGLE_TRANSLATE_API_KEY']
-print(google.translate(text, target_lang=lang))
+def main():
+	lang = sys.argv[1]
+	text = ' '.join(sys.argv[2:])
+
+	google.translate.API_key = os.environ['GOOGLE_TRANSLATE_API_KEY']
+	print(google.translate(text, target_lang=lang))
+
+
+__name__ == '__main__' and main()
