818d92b07eae647c9da06b2703cf066df829fe12
Jason R. Coombs <jaraco@jaraco.com>
2021-02-01 19:12:22 -0500

    ⚫ Fade to black.

--- jaraco/translate/test_google.py
+++ jaraco/translate/test_google.py
@@ -7,13 +7,13 @@ from . import google
 
 @pytest.fixture
 def google_api_key(monkeypatch):
-	key = os.environ.get('GOOGLE_API_KEY')
-	if not key:
-		pytest.skip("Need GOOGLE_API_KEY environment variable")
-	monkeypatch.setattr(google.translate, 'API_key', key, raising=False)
+    key = os.environ.get('GOOGLE_API_KEY')
+    if not key:
+        pytest.skip("Need GOOGLE_API_KEY environment variable")
+    monkeypatch.setattr(google.translate, 'API_key', key, raising=False)
 
 
 def test_translate(google_api_key):
-	# Google seems to title-case Hello World. *shrug*
-	expected = 'Hello World'
-	assert google.translate('hola mundo') == expected
+    # Google seems to title-case Hello World. *shrug*
+    expected = 'Hello World'
+    assert google.translate('hola mundo') == expected
